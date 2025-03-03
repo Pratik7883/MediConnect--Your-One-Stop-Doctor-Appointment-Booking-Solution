df01c4e917670fba2de3b084ec760dbcc0405933
Pratik Shingade <pratik.shingade23@pccoepune.org>
2025-03-03 22:10:43 +0530

    ..
    ..

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const appointmentBtn = document.querySelector(".appointment-btn");
     const userIcon = document.getElementById("user-icon");
     const isLoggedIn = false; // Change to `true` if user is logged in
-
+  
     // Toggle user icon visibility
     if (isLoggedIn) {
         userIcon.classList.remove("hidden");
